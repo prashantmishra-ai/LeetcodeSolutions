97690fca403f5649e4e04990cd989291ff0b30c2
Prashant Mishra <85211105+prashantmishra-ai@users.noreply.github.com>
2023-07-26 09:51:43 +0530

    Time: 3722 ms (34.47%), Space: 30.9 MB (25.64%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.py
@@ -1,11 +1,27 @@
+from typing import List
+
 class Solution:
     def minSpeedOnTime(self, dist: List[int], t: float) -> int:
-        left, right, min_speed = 1, 10_000_000 + 1, -1
+        n = len(dist)
+        if n - 1 > t:
+            return -1  # Impossible to reach in time
+
+        left, right = 1, int(1e9) + 1  # Adjust the right boundary based on the problem constraints
+        min_speed = -1
+
         while left < right:
-            speed = left + (right - left) // 2
-            if (sum((dist[i] + speed - 1) // speed for i in range(len(dist) - 1)) + dist[-1] / speed) > t:
-                left = speed + 1
-            else:
-                right = speed
+            speed = (left + right) // 2
+            time_taken = 0
+
+            for i in range(n - 1):
+                time_taken += (dist[i] + speed - 1) // speed
+
+            time_taken += dist[-1] / speed
+
+            if time_taken <= t:
                 min_speed = speed
+                right = speed
+            else:
+                left = speed + 1
+
         return min_speed
