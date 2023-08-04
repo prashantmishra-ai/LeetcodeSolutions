d65f685a3c86370d01b03fc24957cc9a4940acce
Prashant Mishra <85211105+prashantmishra-ai@users.noreply.github.com>
2023-08-04 19:54:28 +0530

    Time: 126 ms (72.61%), Space: 14 MB (78.92%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,12 +1,33 @@
 class Solution(object):
     def findSubstring(self, s, words):
+        """
+        :type s: str
+        :type words: List[str]
+        :rtype: List[int]
+        """
         from collections import Counter
-        wordBag = Counter(words)
+
+        wordBag = Counter(words)  # Counter for the words array
         wordLen, numWord = len(words[0]), len(words)
-        totalLen = wordLen * numWord 
+        totalLen = wordLen * numWord  # total length of all words
         res = []
-        for i in range(len(s) - totalLen + 1):
-            wordSeen = [s[i+j*wordLen:i+j*wordLen+wordLen] for j in range(numWord)]
-            if Counter(wordSeen) == wordBag:
-                res.append(i)
+
+        for i in range(wordLen):  # i is the offset
+            left = i
+            right = i
+            currCounter = Counter()
+
+            while right + wordLen <= len(s):
+                w = s[right:right + wordLen]
+                right += wordLen
+                currCounter[w] += 1
+
+                while currCounter[w] > wordBag[w]:
+                    left_w = s[left:left+wordLen]
+                    left += wordLen
+                    currCounter[left_w] -= 1
+
+                if right - left == totalLen:
+                    res.append(left)
+                    
         return res
