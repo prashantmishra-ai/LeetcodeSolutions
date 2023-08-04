2ee1f94c1c1de514a9bbfd3bfb26ed962f1a875a
Prashant Mishra <85211105+prashantmishra-ai@users.noreply.github.com>
2023-08-04 19:47:58 +0530

    Time: 27 ms (94.05%), Space: 15.1 MB (41.19%) - LeetHub

---
+++ 0025-reverse-nodes-in-k-group/0025-reverse-nodes-in-k-group.py
@@ -0,0 +1,26 @@
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution(object):
+    def reverseKGroup(self, head, k):
+        count = 0
+        ptr = head
+        while count < k and ptr:
+            ptr = ptr.next
+            count += 1
+        if count == k: 
+            reversedHead, ptr = self.reverseLinkedList(head, k)
+            head.next = self.reverseKGroup(ptr, k)
+            return reversedHead
+        return head
+    def reverseLinkedList(self, head, k):
+        new_head, ptr = None, head
+        while k:
+            next_node = ptr.next
+            ptr.next = new_head
+            new_head = ptr
+            ptr = next_node
+            k -= 1
+        return new_head, ptr
