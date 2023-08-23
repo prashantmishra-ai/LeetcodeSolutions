f985eea4e4fb77b548f8aa3c9266041a50e03fd5
Prashant Mishra <85211105+prashantmishra-ai@users.noreply.github.com>
2023-08-23 08:44:54 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (46.07%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.cpp
+++ 0767-reorganize-string/0767-reorganize-string.cpp
@@ -2,39 +2,40 @@ class Solution {
 public:
     string reorganizeString(string s) {
         vector<int> charCounts(26, 0);
+        int maxCount = 0;
+        char maxChar;
+        
+        // Count the characters and find the character with maximum occurrences
         for (char c : s) {
-            charCounts[c - 'a'] = charCounts[c - 'a'] + 1;
+            if (++charCounts[c - 'a'] > maxCount) {
+                maxCount = charCounts[c - 'a'];
+                maxChar = c;
+            }
         }
 
+        // If any character occurs more than (n + 1) / 2 times, return an empty string
+        if (maxCount > (s.size() + 1) / 2) return "";
+
         // Max heap ordered by character counts
-        priority_queue<vector<int>> pq;
+        priority_queue<pair<int, char>> pq;
         for (int i = 0; i < 26; i++) {
             if (charCounts[i] > 0) {
-                pq.push(vector<int>{charCounts[i], i + 'a'});
+                pq.emplace(charCounts[i], i + 'a');
             }
         }
         
         string result;
+        pair<int, char> prev = {0, ' '};
+
         while (!pq.empty()) {
-            auto first = pq.top();
-            pq.pop();
-            if (result.empty() || first[1] != result.back()) {
-                result += char(first[1]);
-                if (--first[0] > 0) {
-                    pq.push(first);
-                }
-            } else {
-                if (pq.empty()) {
-                    return "";
-                }
-                auto second = pq.top();
-                pq.pop();
-                result += char(second[1]);
-                if (--second[0] > 0) {
-                    pq.push(second);
-                }
-                pq.push(first);
+            auto [count, char_] = pq.top(); pq.pop();
+
+            if (prev.first > 0) {
+                pq.push(prev);
             }
+
+            result.push_back(char_);
+            prev = {count - 1, char_};
         }
 
         return result;
